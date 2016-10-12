348df7ca6e3345e6470d6975e00d615f30b963d9
Anirudhan Jegannathan <anirudhan@rajegannathan.in>
2016-10-11 21:57:08 -0400

    changes for doing a grid search

--- labs/lab1/heatdist.cu
+++ labs/lab1/heatdist.cu
@@ -28,32 +28,24 @@
 #define index(i, j, N)  ((i)*(N)) + (j)
 
 /* Block width definition */
-#define BLOCK_WIDTH 16
+//define BLOCK_WIDTH 16.0
 
 /*****************************************************************/
 
 // Function declarations: Feel free to add any functions you want.
 void  seq_heat_dist(float *, unsigned int, unsigned int);
-void  gpu_heat_dist(float *, unsigned int, unsigned int);
-float  calc_playground(float *, unsigned int);
-
+void  gpu_heat_dist(float *, unsigned int, unsigned int, unsigned int);
+float  sum_playground(float *, unsigned int);
+float* init_playground(unsigned int);
+void do_heat_distribution(unsigned int, unsigned int, unsigned int);
 
 /*****************************************************************/
 
 int main(int argc, char * argv[])
 {
 	unsigned int N; /* Dimention of NxN matrix */
-	int type_of_device = 0; // CPU or GPU
-	int iterations = 0;
-	int i;
-	float sum = 0;
-
-	/* The 2D array of points will be treated as 1D array of NxN elements */
-	float * playground; 
-
-	// to measure time taken by a specific part of the code 
-	double time_taken;
-	clock_t start, end;
+	unsigned int iterations = 0;
+	unsigned int block_sizes[] = {8, 16, 32};
 
 	if(argc != 4)
 	{
@@ -64,10 +56,68 @@ int main(int argc, char * argv[])
 		exit(1);
 	}
 
-	type_of_device = atoi(argv[3]);
+	//type_of_device = atoi(argv[3]);
 	N = (unsigned int) atoi(argv[1]);
 	iterations = (unsigned int) atoi(argv[2]);
 
+	printf("N \t cpu_time \t gpu_time \t cpu_sum \t gpu_sum");
+
+	N = 100;
+	while(N < 10 * 1000){
+		for(int i = 0; i < 3; i++){
+			do_heat_distribution(N, block_sizes[i], iterations);
+		}
+		N = N * 2;
+	}
+
+	return 0;
+}
+
+void do_heat_distribution(unsigned int N, unsigned int block_size, unsigned int iterations){
+	float* playground;
+	float gpu_sum = 0, cpu_sum = 0;
+
+	// to measure time taken by a specific part of the code 
+	double cpu_time, gpu_time;
+	clock_t start, end;
+
+	playground = init_playground(N);
+	//printf("sum is %f \n", sum_playground(playground, N));
+	start = clock();
+	seq_heat_dist(playground, N, iterations);
+	end = clock();
+
+	cpu_time = ((double) (end - start));
+	//printf("Time taken for %s is %lf\n", "CPU", cpu_time);
+
+	cpu_sum = sum_playground(playground, N);
+	//printf("Sum is %f\n", cpu_sum);
+
+	free(playground);
+
+	playground = init_playground(N);
+	//printf("sum is %f \n", sum_playground(playground, N));
+	start = clock();
+	gpu_heat_dist(playground, N, iterations, block_size); 
+	end = clock();    
+
+	gpu_time = ((double) (end - start));
+	//printf("Time taken for %s is %lf\n", "GPU", gpu_time);
+
+	gpu_sum = sum_playground(playground, N);
+	//printf("Sum is %f\n", gpu_sum);
+
+	free(playground);
+
+	printf("%d \t %lf \t %lf \t %f \t %f \n", N, cpu_time, gpu_time, cpu_sum, gpu_sum);
+
+}
+
+float* init_playground(unsigned int N){
+	/* The 2D array of points will be treated as 1D array of NxN elements */
+	float * playground; 
+
+	int i;
 
 	/* Dynamically allocate NxN array of floats */
 	playground = (float *)calloc(N*N, sizeof(float));
@@ -81,66 +131,29 @@ int main(int argc, char * argv[])
 	// Edge elements to 80F
 	for(i = 0; i < N; i++){
 		playground[index(0,i,N)] = 80;
-		//printf("%f ",playground[index(0, i, N)]);
 	}
-	//printf("\n");
 
 	for(i = 0; i < N; i++){
 		playground[index(i,0,N)] = 80;
-		//printf("%f ",index(i, 0, N));
 	}
-	//printf("\n");
 
 	for(i = 0; i < N; i++){
 		playground[index(i,N-1, N)] = 80;
-		//printf("%f ",index(i, N-1, N));
 	}
-	//printf("\n");
 
 	for(i = 0; i < N; i++){
 		playground[index(N-1,i,N)] = 80;
-		//printf("%f ",index(N-1, i, N));
 	}
-	//printf("\n");
 
 	// from (0,10) to (0,30) inclusive are 150F
-	//for(i = 10; i <= 30; i++){
-		//playground[index(i,0,N)] = 150;
-		//printf("%f ",playground[index(i, 0, N)]);
-	//}
-	//printf("\n");
-
-	sum = calc_playground(playground, N);
-	printf("Sum is %f\n", sum);
-
-	if( !type_of_device ) // The CPU sequential version
-	{  
-		start = clock();
-		seq_heat_dist(playground, N, iterations);
-		end = clock();
+	for(i = 10; i <= 30 && i < N; i++){
+		playground[index(i,0,N)] = 150;
 	}
-	else  // The GPU version
-	{
-		start = clock();
-		gpu_heat_dist(playground, N, iterations); 
-		end = clock();    
-	}
-
-
-	time_taken = ((double)(end - start))/ CLOCKS_PER_SEC;
-
-	printf("Time taken for %s is %lf\n", type_of_device == 0? "CPU" : "GPU", time_taken);
-
-	sum = calc_playground(playground, N);
-	printf("Sum is %f\n", sum);
-
-	free(playground);
-
-	return 0;
-
+	
+	return playground;
 }
 
-float  calc_playground(float* playground, unsigned int N){
+float  sum_playground(float* playground, unsigned int N){
 	int i;
 	float sum = 0.0;
 	for(i = 0; i < N*N; i++){
@@ -194,7 +207,7 @@ void  seq_heat_dist(float * playground, unsigned int N, unsigned int iterations)
 }
 
 __global__
-void calculate_temperature(float* d_temp, float* d_playground, int N){
+void calculate_temperature(float* d_playground, float* d_temp, int N){
 	int row = blockIdx.y * blockDim.y + threadIdx.y;
 	int col = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -202,39 +215,39 @@ void calculate_temperature(float* d_temp, float* d_playground, int N){
 		return;
 	}
 
-	d_temp[index(row, col, N)] = (d_playground[index(row-1, col, N)] + d_playground[index(row+1, col, N)] + d_playground[index(row, col + 1, N)] + d_playground[index(row, col-1, N)])/4.0;
-	//printf("%d %d :- %f  %d %d %d %d \n", row, col, d_temp[index(row, col, N)], index(row + 1, col, N), index(row - 1, col, N), index(row, col - 1, N), index(row, col + 1, N) );
-
+	d_temp[index(row, col, N)] = (d_playground[index(row-1, col, N)] + 
+					d_playground[index(row+1, col, N)] + 
+					d_playground[index(row, col + 1, N)] + 
+					d_playground[index(row, col-1, N)])/4.0;
 }
 
 /***************** The GPU version: Write your code here *********************/
 __host__
-void  gpu_heat_dist(float * playground, unsigned int N, unsigned int iterations)
+void  gpu_heat_dist(float * playground, unsigned int N, unsigned int iterations, unsigned int BLOCK_WIDTH)
 {
 	// Loop indices
 	//int i, j, k;
 	//int upper = N-1;
 	int iter = iterations;
 
-	int size = N*N;
+	int size = N*N*sizeof(float);
 	float *d_playground, *d_temp;
 
-	cudaMalloc((void **) &d_playground, size * sizeof(float));
-	cudaMalloc((void **) &d_temp, size *sizeof(float));
+	cudaMalloc((void **) &d_playground, size);
+	cudaMalloc((void **) &d_temp, size );
 
-	cudaMemcpy(d_playground, playground, size*sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_temp, playground, size*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_playground, playground, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_temp, playground, size, cudaMemcpyHostToDevice);
 
-	dim3 dimGrid(ceil(N*N/256.0), 1, 1);
-	dim3 dimBlock(16, 16, 1);
-	printf("N is %d \n",N);
+	dim3 dimGrid(ceil(N/(float)BLOCK_WIDTH), ceil(N/(float)BLOCK_WIDTH), 1);
+	dim3 dimBlock(BLOCK_WIDTH, BLOCK_WIDTH, 1);
 
 	while(iter > 0){
-		calculate_temperature<<<dimGrid, dimBlock>>>(d_temp, d_playground, N);
-		cudaMemcpy(d_playground, d_temp, size*sizeof(float), cudaMemcpyDeviceToDevice);
+		calculate_temperature<<<dimGrid, dimBlock>>>(d_playground, d_temp, N);
+		cudaMemcpy(d_playground, d_temp, size, cudaMemcpyDeviceToDevice);
 		iter = iter -1;
 	}
-	cudaMemcpy(playground, d_temp, size*sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(playground, d_playground, size, cudaMemcpyDeviceToHost);
 
 	cudaFree(d_temp);
 	cudaFree(d_playground);
