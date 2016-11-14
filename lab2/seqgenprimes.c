55c468420dc02651dd2d25a3fcf2da53e1b45d9e
Anirudhan J. rajagopalan <ajr619@nyu.edu>
2016-11-14 11:36:58 -0500

    skeleton

--- lab2/seqgenprimes.c
+++ lab2/seqgenprimes.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int main(char* argv){
+int main(int argc, char** argv){
+    if(argc != 2){
+        printf("not enough arguments");
+        exit(0);
+    }
 
+    int N = atoi(argv[1]);
+    printf("Input n is %d \n", N);
 }
