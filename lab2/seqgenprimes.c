836ff1db8344eb6b6d144a037c831514af25dfa9
Anirudhan J. rajagopalan <ajr619@nyu.edu>
2016-11-14 18:50:03 -0500

    changes for finding primes sequentially

--- lab2/seqgenprimes.c
+++ lab2/seqgenprimes.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+typedef int TYPE;
+
+int findNextPrime(TYPE* arr, int length, int lastPrime){
+    int i = lastPrime + 1;
+    for(; i < length; i++){
+        if(arr[i-1] == 0){
+            break;
+        }
+    }
+    //printf("Returning %d\n", i);
+    return i;
+}
+
 int main(int argc, char** argv){
     if(argc != 2){
         printf("not enough arguments");
@@ -8,5 +21,22 @@ int main(int argc, char** argv){
     }
 
     int N = atoi(argv[1]);
-    printf("Input n is %d \n", N);
+    TYPE* arr = (TYPE*)calloc(N*(sizeof(TYPE)), 1);
+    arr[0] = 1;
+
+    int lastPrime = 1;
+    while(lastPrime < N){
+        int prime = findNextPrime(arr, N, lastPrime);
+        for(int i = prime * 2; i <= N; i = i + prime){
+            arr[i - 1] = 1;
+            //printf("prime is %d. Index is %d.  Value is %d\n", prime, i, arr[i-1]);
+        }
+        lastPrime = prime;
+    }
+
+    for(int i = 0; i < N; i++){
+        if(arr[i] == 0){
+            printf("%d ", i+1);
+        }
+    }
 }
