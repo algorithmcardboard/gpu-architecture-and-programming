8b80c71b48fabab3c3a34660d73859ffe9a092c0
Anirudhan Jegannathan <anirudhan@rajegannathan.in>
2016-11-15 20:44:06 -0500

    just the psuedo code

--- lab2/genprimes.cu
+++ lab2/genprimes.cu
@@ -14,7 +14,9 @@
 
 typedef int TYPE;
 
-void do_gpu_seieve(TYPE*, int);
+TYPE* find_primes(unsigned int);
+void do_gpu_seieve(TYPE*, unsigned int);
+void fill_zeros(TYPE*, unsigned int);
 
 int main(int argc, char * argv[]){
     unsigned int N;
@@ -25,4 +27,28 @@ int main(int argc, char * argv[]){
     }
 
     N = atoi(argv[1]);
+
+    find_primes(N);
+}
+
+__host__ __device__
+void fill_zeros(TYPE* arr, unsigned int N){
+    unsigned int i = 0;
+    for(i= 0; i < N; i++){
+        arr[i] = 0;
+    }
+}
+
+__host__
+TYPE* find_primes(unsigned int N){
+    unsigned int i  = 0;
+    TYPE* arr = (TYPE*) malloc(N * sizeof(TYPE));
+    fill_zeros(arr);
+    // Find all primes till sqrt(n)
+
+    // Call threads with two lists.  One with array of numbers.  The other with all primes till sqrt(n).
+}
+
+__global__
+void do_gpu_seieve(TYPE* arr, unsigned int N){
 }
