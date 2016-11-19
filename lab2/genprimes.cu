f2ab57a1f79e6d89a1e11acbf16139e59d7cd3cd
Anirudhan Jegannathan <anirudhan@rajegannathan.in>
2016-11-19 00:42:06 -0500

    almost working version

--- lab2/genprimes.cu
+++ lab2/genprimes.cu
@@ -24,6 +24,7 @@ void fill_zeros(TYPE*, unsigned int);
 
 int main(int argc, char * argv[]){
     unsigned int N;
+    int i = 0;
 
     if(argc != 2){
         printf("Invoke with just one argument (N) that says the maximum value till which to generate primes.\n");
@@ -32,7 +33,18 @@ int main(int argc, char * argv[]){
 
     N = atoi(argv[1]);
 
-    find_primes(N, 1);
+    //printf("N is %d\n", N);
+
+    unsigned int count = 0;
+
+    TYPE* arr = find_primes(N, 20);
+    for(i = 0; i < N; i++){
+        if(arr[i] == 0){
+            printf("%d ", i + 1);
+            count = count + 1;
+        }
+    }
+    printf("Count is %d\n", count);
 }
 
 void fill_zeros(TYPE* arr, unsigned int N){
@@ -40,16 +52,18 @@ void fill_zeros(TYPE* arr, unsigned int N){
     for(i= 0; i < N; i++){
         arr[i] = 0;
     }
+    //printf("Filling zeros");
 }
 
 unsigned int* find_next_primes(unsigned int* arr, unsigned int* primes, unsigned int last_prime, int k, unsigned int N){
     int i = 0;
-    int j = last_prime;
-    for(j = last_prime; j < N && i < k; j++){
+    int j = last_prime + 1;
+    for(; j < N && i < k; j++){
         if(arr[j] == 0){
-            primes[i++] = j + 1;
+            primes[i++] = j;
         }
     }
+    //printf("\n");
 
     return primes;
 }
@@ -57,33 +71,38 @@ unsigned int* find_next_primes(unsigned int* arr, unsigned int* primes, unsigned
 __host__
 TYPE* find_primes(unsigned int N, int k){
     TYPE* arr = (TYPE*) malloc(N * sizeof(TYPE));
+    unsigned int* primes = (unsigned int*)malloc(k*sizeof(int));
+    unsigned int last_prime = 0;
+
     fill_zeros(arr, N);
+    arr[0] = 1;
 
 	TYPE *d_arr, *d_primes;
 
 	cudaMalloc((void **) &d_arr, N*sizeof(TYPE));
 	cudaMalloc((void **) &d_primes, k*sizeof(int));
+    //printf("allocated device memory \n");
 
 	cudaMemcpy(d_arr, arr, N*sizeof(TYPE), cudaMemcpyHostToDevice);;
+
+    //printf("Copied to device \n");
 
 	dim3 gridDimension(ceil(N/(float)BLOCK_WIDTH), 1, 1);
 	dim3 blockDimension(BLOCK_WIDTH, 1, 1);
 
     int i = 0;
-    d_arr[0] = 1;
-
-    unsigned int last_prime = 1;
-    unsigned int* primes = (unsigned int*)malloc(k*sizeof(int));
 
     do{
-        primes = find_next_primes(arr, primes, last_prime, k, N);
+        find_next_primes(arr, primes, last_prime, k, N);
         for(i = 0; i < k; i++){
             if(*(primes + i) > last_prime){
                 last_prime = *(primes + i);
             }
         }
-        cudaMemcpy(d_primes, primes, N*sizeof(int), cudaMemcpyHostToDevice);
+        //printf("primes is %d.  Last prime is %d\n", primes[0], last_prime);
+        cudaMemcpy(d_primes, primes, k*sizeof(int), cudaMemcpyHostToDevice);
         do_seieve<<<gridDimension, blockDimension>>>(d_arr, d_primes, N, k);
+        cudaMemcpy(arr, d_arr, N*sizeof(TYPE), cudaMemcpyDeviceToHost);
     }while(last_prime < (N+1)/2);
 
     cudaMemcpy(arr, d_arr, N*sizeof(TYPE), cudaMemcpyDeviceToHost);
@@ -92,12 +111,18 @@ TYPE* find_primes(unsigned int N, int k){
 
 __global__
 void do_seieve(TYPE* d_arr, unsigned int* d_primes, unsigned int N, int k){
-	int id = blockIdx.x * blockDim.x + threadIdx.x;
 
-    int i = 0;
+    int i, id = blockIdx.x * blockDim.x + threadIdx.x + 1;
+
+    if(id > N){
+        return;
+    }
+
+
     for(i = 0; i < k; i++){
-        if(id % d_primes[k] == 0){
-            d_arr[id] = 1;
+        if(id != (d_primes[i]+1) && (id % (d_primes[i]+1) == 0)){
+            d_arr[id -1] = 1;
+            //printf("id is %d. prime is %d\n", id, d_primes[i]+1);
         }
     }
 }
