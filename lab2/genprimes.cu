e4e05b70ee4285b5ad7092341e88fcefae0db86f
Anirudhan Jegannathan <anirudhan@rajegannathan.in>
2016-11-18 21:48:03 -0500

    few more changes

--- lab2/genprimes.cu
+++ lab2/genprimes.cu
@@ -13,10 +13,12 @@
 #include <time.h> 
 #include <math.h> 
 
-typedef int TYPE;
+typedef unsigned int TYPE;
+
+#define BLOCK_WIDTH 1024;
 
 TYPE* find_primes(unsigned int);
-void do_gpu_seieve(TYPE*, unsigned int);
+void do_seieve(TYPE*, unsigned int);
 void fill_zeros(TYPE*, unsigned int);
 
 int main(int argc, char * argv[]){
@@ -40,20 +42,30 @@ void fill_zeros(TYPE* arr, unsigned int N){
     }
 }
 
-int* findAllPrimes(unsigned int N){
-}
-
 __host__
 TYPE* find_primes(unsigned int N){
     unsigned int i  = 0;
     TYPE* arr = (TYPE*) malloc(N * sizeof(TYPE));
     fill_zeros(arr);
-    int* primes = find_all_primes(sqrt(N));
-    // Find all primes till sqrt(n)
 
-    // Call threads with two lists.  One with array of numbers.  The other with all primes till sqrt(n).
+    int* primes = find_all_primes(sqrt(N));
+
+	TYPE *d_arr;
+
+	cudaMalloc((void **) &d_arr, N*sizeof(TYPE));
+
+	cudaMemcpy(d_arr, arr, N*sizeof(TYPE), cudaMemcpyHostToDevice);
+
+	dim3 gridDimension(ceil(N/(float)BLOCK_WIDTH), 1, 1);
+	dim3 blockDimension(BLOCK_WIDTH, 1, 1);
+
+    unsigned int nextPrime = 1;
+    unsigned int stopValue = (N+1)/2 + 1;
+
+    do{
+    }while(nextPrime < (N+1)/2);
 }
 
 __global__
-void do_gpu_seieve(TYPE* arr, unsigned int N){
+void do_seieve(TYPE* arr, unsigned int N){
 }
